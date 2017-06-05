3b418646355960ac7c3bd03f31ed968d19abb321
srikanthpragada <srikanthpragada@gmail.com>
2017-06-05 11:28:12 +0530

    5-JUNE

---
+++ Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            User u = new User();
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                if (user.UserName == "admin" && user.Password == "admin")
+                   return RedirectToAction("Index");
+                else
+                {
+                    ViewBag.Message = "Invalid Login!";
+                }
+            }
+
+            return View(user);
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcdemo.Models
+{
+    public class User
+    {
+        [Required]
+        [StringLength(10, MinimumLength = 4, ErrorMessage ="Username must be 4 to 10 chars")]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "Password must be 4 to 8 chars")]
+        public string Password { get; set; }
+    }
+}
